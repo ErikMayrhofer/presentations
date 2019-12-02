95aaf770874f6780ec73687e111fcc5ee5ce9061
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-12-02 19:01:25 +0100

    Added Ktor

--- presentations/django/code/ktorquote/src/Application.kt
+++ presentations/django/code/ktorquote/src/Application.kt
@@ -4,12 +4,17 @@ import at.htl.model.Person
 import at.htl.model.Quote
 import io.ktor.application.*
 import io.ktor.features.ContentNegotiation
+import io.ktor.html.respondHtml
 import io.ktor.jackson.jackson
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.p
+import kotlinx.html.ul
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -20,7 +25,7 @@ val persons = mutableListOf(
 
 val quotes = mutableListOf(
     Quote(1).apply{text="SomeQuoteTest"; person=persons[0]},
-    Quote(2).apply{text="SomeQuoteTest"; person=persons[0]}
+    Quote(2).apply{text="QuoteTest"; person=persons[0]}
 )
 
 fun insertQuote(quote: Quote): Quote{
@@ -44,6 +49,22 @@ fun Application.module(testing: Boolean = false) {
             val quote = call.receive<Quote>()
             insertQuote(quote)
             call.respond(quote)
+        }
+        get("/quoteui") {
+            call.respondHtml {
+                body {
+                    h1 {
+                        +"Quotes"
+                    }
+                    quotes.forEach {
+                        ul {
+                            p {
+                            +"Quote: ${it.text}"
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
